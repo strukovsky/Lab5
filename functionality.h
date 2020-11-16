e99feaf1915d979a99a9a1d0697c735ef82fdf0b
strukovsky <strukovsky1@gmail.com>
2020-11-16 11:34:41 +0300

    init

---
+++ functionality.h
@@ -0,0 +1,14 @@
+//
+// Created by strukovsky on 16.11.2020.
+//
+
+#ifndef LAB5_FUNCTIONALITY_H
+#define LAB5_FUNCTIONALITY_H
+
+template <class T>
+void fill(T array[], int length);
+
+template <class T>
+T sum_of_less_than(T array[], int length, T n);
+
+#endif //LAB5_FUNCTIONALITY_H
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "functionality.cpp"
+
+
+int main() {
+
+    int *arr_int = new int[5];
+    double *arr_double = new double [5];
+    char *arr_char = new char[5];
+    std::cout<<"Enter int array\n";
+    fill<int>(arr_int, 5);
+    for (int i = 0; i < 5; ++i) {
+        std::cout<<arr_int[i]<<" ";
+    }
+    std::cout<<"\n";
+    std::cout<<"Enter double array\n";
+    fill(arr_double, 5);
+    for (int i = 0; i < 5; ++i) {
+        std::cout<<arr_double[i]<<" ";
+    }
+    std::cout<<"\n";
+    std::cout<<"Entering char array\n";
+    fill(arr_char, 5);
+    for (int i = 0; i < 5; ++i) {
+        std::cout<<arr_char[i]<<" ";
+    }
+    std::cout<<"\n";
+    std::cout<<"Calculating sum of array\n";
+    int n_for_int;
+    std::cout<<"Enter n for int array\n";
+    std::cin>>n_for_int;
+    std::cout<<"Sum of int "<<sum_of_less_than(arr_int, 5, n_for_int)<<"\n";
+    double n_for_double;
+    std::cout<<"Enter n for double array\n";
+    std::cin>>n_for_double;
+    std::cout<<"Sum of double "<<sum_of_less_than(arr_double, 5, n_for_double)<<"\n";
+    char n_for_char;
+    std::cout<<"Enter n for char array\n";
+    std::cin>>n_for_char;
+    std::cout<<"Sum of char "<<sum_of_less_than(arr_char, 5, n_for_char)<<"\n";
+    
+
+
+    return 0;
+}
